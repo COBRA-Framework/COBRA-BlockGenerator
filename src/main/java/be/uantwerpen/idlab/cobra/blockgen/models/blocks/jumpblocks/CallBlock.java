d6887f992465acfb026c71f15530cc20a1e0ace9
Thomas Huybrechts <Thomas.Huybrechts@uantwerpen.be>
2017-01-23 14:51:55 +0100

    Fixed typo in window header Added Call Block Class (not implemented yet)

---
+++ src/main/java/be/uantwerpen/idlab/cobra/blockgen/models/blocks/jumpblocks/CallBlock.java
@@ -0,0 +1,52 @@
+package be.uantwerpen.idlab.cobra.blockgen.models.blocks.jumpblocks;
+
+import be.uantwerpen.idlab.cobra.blockgen.models.CodeSegment;
+import be.uantwerpen.idlab.cobra.blockgen.models.blocks.JumpBlock;
+import be.uantwerpen.idlab.cobra.blockgen.models.xml.XMLElement;
+import be.uantwerpen.idlab.cobra.blockgen.models.xml.XMLObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Thomas on 23/01/2017.
+ */
+//TODO: Not implemented yet!
+public class CallBlock extends JumpBlock
+{
+    private String calledSection;
+
+    protected CallBlock()
+    {
+        super();
+    }
+
+    public CallBlock(String calledSection, CodeSegment codeSegment)
+    {
+        super(codeSegment);
+
+        this.calledSection = calledSection;
+    }
+
+    public String getCalledSection()
+    {
+        return this.calledSection;
+    }
+
+    public void setCalledSection(String calledSection)
+    {
+        this.calledSection = calledSection;
+    }
+
+    @Override
+    public List<XMLObject> getXMLElements()
+    {
+        List<XMLObject> elements = new ArrayList<XMLObject>();
+
+        //Element: called section value
+        elements.add(new XMLElement("called_section", this.calledSection));
+        elements.addAll(super.getXMLElements());
+
+        return elements;
+    }
+}
